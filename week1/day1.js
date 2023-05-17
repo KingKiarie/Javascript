f962e4f94ead08a4df129ef1ac9393a867e5cebc
KingKiarie <kiarie7mbugua@gmail.com>
2023-05-17 12:04:12 +0300

    dy1.js area added.

--- week1/day1.js
+++ week1/day1.js
@@ -1,11 +1,21 @@
-//
+//periemeter of a rectangle
 let a = prompt('Enter first number:');
 let b = prompt('Enter second number:');
 
 function perimeter(){
-    let result = 2(parseInt(a)+parseInt(b));
+    let result = 2*(parseInt(a)+parseInt(b));
     console.log(result);
 }
 
 perimeter();
+//area of a rectangle
 
+let num1 = prompt('Enter first number:');
+let num2 = prompt('Enter second number');
+
+function area(){
+    let result = parseInt(num1)* parseInt(num2);
+    console.log(result);
+} 
+
+area();

79dfe692b099ab81688a54bb50c37b237fc97707
KingKiarie <kiarie7mbugua@gmail.com>
2023-05-17 12:00:20 +0300

    Modified day1&2

---
+++ week1/day1.js
@@ -0,0 +1,11 @@
+//
+let a = prompt('Enter first number:');
+let b = prompt('Enter second number:');
+
+function perimeter(){
+    let result = 2(parseInt(a)+parseInt(b));
+    console.log(result);
+}
+
+perimeter();
+
